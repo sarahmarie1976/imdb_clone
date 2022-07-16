aa6c0f868c860f9f3bb9da939c28b42d0d38568a
Sarahmarie D Hollenstein <sholle7@gmail.com>
2022-07-16 13:35:36 -0500

    :sparkles: Created Header component and icons

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from 'next/dist/client/image';
+import React from 'react';
+import HeaderIcon from './HeaderIcon';
+import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="">
+    <div className="flex">
+      <HeaderIcon Icon={HomeIcon} title="HOME" />
+      <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
+      <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+      <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+      
     </div>
-  )
+    <Image
+      src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+      width={100}
+      height={100}
+      className="cursor-pointer active:brightness-110"
+    />
+  </div>
+);
 }
---
+++ components/HeaderIcon.js
@@ -0,0 +1,8 @@
+export default function HeaderIcon({Icon, title}) {
+  return (
+      <div className="">
+          <Icon className="h-8"/>
+          <p className="">{title}</p>
+      </div>
+  )
+}
